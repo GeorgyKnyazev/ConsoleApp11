83e7da0cfc7308984d557397ebe272f99ac4670b
Георгий Князев <saint_georgy@yahoo.com>
2023-07-12 11:29:56 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrayOfString1 = { "1", "2", "1" };
+            string[] arrayOfString2 = { "3", "2", "5", "7" };
+
+            Stack<string> stackOfString = new Stack<string>();
+
+            Test(arrayOfString1, stackOfString);
+            Test(arrayOfString2, stackOfString);
+
+            while (stackOfString.Count > 0)
+            {
+                Console.WriteLine(stackOfString.Pop());
+            }
+            Console.ReadLine();
+        }
+
+        static void Test(string[] array, Stack<string> stack)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (stack.Contains(array[i]))
+                {
+                    continue;
+                }
+
+                stack.Push(array[i]);
+            }
+        }
+    }
+}
